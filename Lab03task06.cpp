789f8b90548a3b3beedd0956063a897797aab1d9
tasbihanaeem-2512198 <tasbihanaeem64@gmail.com>
2025-12-22 19:16:57 +0500

    Add files via upload

---
+++ Lab03task06.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"Enter the value of a,b and c:";
+cin>>a;
+cin>>b;
+cin>>c;
+float realPart,imagPart;
+double discriminant=b*b-4*a*c;
+double root1,root2;
+if(discriminant>0){
+	root1=-b+sqrt(discriminant)/2*a;
+	root2=-b-sqrt(discriminant)/2*a;
+	cout<<"Two real and distinct roots."<<endl;
+	cout<<"root1="<<root1<<endl;
+	cout<<"root2="<<root2;
+}
+else if(discriminant==0){
+	root1=-b/(2*a);
+	cout<<"Two real and equal roots."<<endl;
+	cout<<"Root="<<root1<<endl;	
+}
+else{
+	realPart=-b/(2*a);
+	imagPart=sqrt(-discriminant)/2*a;
+	cout<<"Roots are imaginary."<<endl;
+	cout<<"Root 1="<<realPart<<"+"<<imagPart<<"i"<<endl;
+	cout<<"Root 2="<<realPart<<"-"<<imagPart<<"i"<<endl;
+}
+return 0;
+}

85968da9d0cc6fc7129467bb4013477dddd2f1c2
Tasbiha Naeem <tasbihanaeem64@gmail.com>
2025-12-23 04:12:59 +0500

    Update Lab04task01.cpp

--- Lab04task01.cpp
+++ Lab04task01.cpp
@@ -1,3 +1,9 @@
+/*1. Ali went to visit his grandmother in USA and she gave him 368 pennies as a gift. 
+Carrying so many coins would be a hassle, so Ali wishes to convert the 368 pennies 
+into a better format. Write a C program which converts the 368 pennies into a set of 
+pennies, nickels, dimes, and quarters such that Ali has to carry the least amount of 
+coins.  1 quarter = 25 pennies  1 dime = 10 pennies  1 nickel = 5 pennies*/
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -21,4 +27,5 @@ int main(){
 	
 	
 	return 0;
+
 }

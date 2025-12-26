9bf57e219f78061008a71a579cc64fa5bb4c31fd
Tasbiha Naeem <tasbihanaeem64@gmail.com>
2025-12-26 15:39:10 +0500

    Update Lab05task06.cpp

--- Lab05task06.cpp
+++ Lab05task06.cpp
@@ -1,3 +1,12 @@
+/*Using IF and Switch statement, write a program that displays the following menu for the food items 
+available to take order from the customer: ∙ B= Burger (Rs. 200)
+∙ F= French Fries (Rs. 50)
+∙ P= Pizza (Rs. 500)
+∙ S= Sandwiches (Rs. 150)
+The costumer can order any combination of available food. The program first ask to enter the no of 
+types of snacks i.e. 2, 3 or 4 then it ask to enter the choice i.e. B for Burger and then for quantity. The 
+program should finally display the total charges for the order.*/
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -46,4 +55,5 @@ int main(){
 	}
      cout<<"Total Charges:"<<total_charges<<endl;
 	return 0;
+
 }
